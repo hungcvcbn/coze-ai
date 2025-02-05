eb6a68bd1c0ffe74600d54cbf215d94a04ae87de
Nguyen Duc Viet <vietnguyen@authenticeducationhub.com>
2025-02-05 21:49:07 +0700

    add key editor

--- src/components/control-panel-detail/feature/OpeningQuestion.tsx
+++ src/components/control-panel-detail/feature/OpeningQuestion.tsx
@@ -1,30 +1,10 @@
-import React, { useState } from "react";
-import { CKEditor } from "@ckeditor/ckeditor5-react";
-import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+import React, { useState, useRef } from "react";
+import { Editor } from "@tinymce/tinymce-react";
+
 const OpeningQuestion = () => {
   const [editorContent, setEditorContent] = useState(
     `As a professional chef, I have spent years refining my skills in the kitchen. Let me guide you through the art of culinary creation.`
   );
-
-  const editorConfiguration = {
-    toolbar: [
-      "heading",
-      "|",
-      "bold",
-      "italic",
-      "|",
-      "bulletedList",
-      "numberedList",
-      "|",
-      "link",
-      "insertImage",
-      "|",
-      "undo",
-      "redo",
-    ],
-    licenseKey:
-      "eyJhbGciOiJFUzI1NiJ9.eyJleHAiOjE3Mzg3MTM1OTksImp0aSI6IjY1MTcwOGExLTM4NTQtNDE1Ny05Y2RhLTM3YWM1NmQzYTZhMyIsInVzYWdlRW5kcG9pbnQiOiJodHRwczovL3Byb3h5LWV2ZW50LmNrZWRpdG9yLmNvbSIsImRpc3RyaWJ1dGlvbkNoYW5uZWwiOlsiY2xvdWQiLCJkcnVwYWwiLCJzaCJdLCJ3aGl0ZUxhYmVsIjp0cnVlLCJsaWNlbnNlVHlwZSI6InRyaWFsIiwiZmVhdHVyZXMiOlsiKiJdLCJ2YyI6ImZiYjZlZDBkIn0.z9jpXqk_0xL63MloHTLujo8fvSuVW6Z4S0XrLfDKygHIUCpwTpk-ZIXHx5AuPn68TYK4cXMh2DRgIp2R0zxZ1g",
-  };
 
   return (
     <div className='flex flex-col gap-4'>
@@ -32,13 +12,39 @@ const OpeningQuestion = () => {
       <div>
         <div className='text-gray-600 mb-2'>Opening text</div>
         <div className='border rounded-md'>
-          <CKEditor
-            editor={ClassicEditor}
-            config={editorConfiguration}
-            data={editorContent}
-            onChange={(event, editor) => {
-              const data = editor.getData();
-              setEditorContent(data);
+          <Editor
+            apiKey='f8pjoh5n2s3es73zdc0s0li6bip329c0c8wcos51uh3l22f3'
+            initialValue={editorContent}
+            init={{
+              height: 300,
+              menubar: false,
+              plugins: [
+                "advlist",
+                "autolink",
+                "lists",
+                "link",
+                "image",
+                "charmap",
+                "anchor",
+                "searchreplace",
+                "visualblocks",
+                "code",
+                "fullscreen",
+                "insertdatetime",
+                "media",
+                "table",
+                "preview",
+                "help",
+                "wordcount",
+              ],
+              toolbar:
+                "undo redo | blocks | " +
+                "bold italic | alignleft aligncenter " +
+                "alignright alignjustify | bullist numlist outdent indent | " +
+                "removeformat | help",
+            }}
+            onEditorChange={content => {
+              setEditorContent(content);
             }}
           />
         </div>

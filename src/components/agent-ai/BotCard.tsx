8f92c6b2e6ce00208b52d426765eb77e15d79f80
Viet Nguyen <vietnguyen@authenticeducationhub.com>
2025-03-18 17:00:39 +0700

    update

--- src/components/agent-ai/BotCard.tsx
+++ src/components/agent-ai/BotCard.tsx
@@ -40,7 +40,9 @@ const BotCard: React.FC<BotCardProps> = ({
               <Tooltip title={bot?.name}>
                 <div className='font-medium text-16-24 text-neutral line-clamp-2'>{bot?.name}</div>
               </Tooltip>
-              <span className='text-[13px] text-gray-500'>{bot.description}</span>
+              <span className='text-[13px] text-gray-500 line-clamp-4' title={bot.description}>
+                {bot.description}
+              </span>
             </div>
           </div>
         </div>

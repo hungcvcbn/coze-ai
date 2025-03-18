0e61990ade9e8ffd4f69f508098fc1432178e164
Viet Nguyen <vietnguyen@authenticeducationhub.com>
2025-03-18 11:58:49 +0700

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
 "use client";
 
 import { redirect } from "next/navigation";
+import { useAppSelector } from "@/redux/hooks";
 
 const HomePage = () => {
+  const { profile } = useAppSelector(state => state.common);
+  if (profile) {
+    return redirect("/control-panel");
+  }
   return redirect("/login");
 };
 
